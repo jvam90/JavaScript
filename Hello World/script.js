dcbc58c4c011298c1e6c01876759355c4ecb500b
jvam90 <jvam90@gmail.com>
2022-07-26 14:39:27 -0300

    Commit inicial - hello world com JavaScript.

---
+++ Hello World/script.js
@@ -0,0 +1,8 @@
+//Para declarar uma variável, basta seguir a sintaxe:
+//var <nome_variável> = <algum_valor> - o valor é opcional.
+var nome = "João";
+
+//Para imprimir no console, basta usar a função console.log()
+//Quando se fala em console, no navegador seria o equivalente ao console do desenvolvedor
+//No node, seria o console do terminal onde o processo está rodando
+console.log("Olá, " + nome);
